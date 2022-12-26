7155bcd3c28a849997f7316ebce42ceaaf0639c7
Yu Hang <lime1900@126.com>
2022-12-26 14:33:30 +0800

    first commit

---
+++ src/test/java/com/lime/javanio/JavaNioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lime.javanio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
